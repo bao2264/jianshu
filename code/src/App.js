2fb48a575df7f82e47fbaabd8f2d6a780eb02e90
bjw <bjw2264@qq.com>
2021-04-26 15:39:15 +0800

    完善接口验证

--- code/src/App.js
+++ code/src/App.js
@@ -6,165 +6,21 @@ import {
   HStack,
   Center,
   Text,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  Checkbox,
-  Button,
   Flex,
-  Link,
 } from '@chakra-ui/react';
-import { css } from '@emotion/react';
-import { HiUser, HiLockClosed, HiPhone } from 'react-icons/hi';
-import { FaWeibo, FaWeixin, FaQq } from 'react-icons/fa';
+
+import Login from './Login';
+import Register from './Register';
 
 import jsLogo from './jslogo.png';
 import jsSlogan from './jsslogan.png';
 import qr from './qr.png';
 
-function Login() {
-  return (
-    <>
-      <Box
-        border="1px solid #c8c8c8"
-        borderRadius="4px"
-        bg="rgba(181,181,181,0.1)"
-        w="100%"
-      >
-        <InputGroup>
-          <InputLeftElement h="50px" children={<HiUser />} />
-          <Input
-            type="text"
-            h="50px"
-            fontSize="13px"
-            placeholder="手机号或邮箱"
-            focusBorderColor="inherit"
-            border="none"
-            borderBottom="1px solid #c8c8c8"
-            borderRadius="0px"
-          />
-        </InputGroup>
-        <InputGroup>
-          <InputLeftElement h="50px" children={<HiLockClosed />} />
-          <Input
-            type="password"
-            h="50px"
-            fontSize="13px"
-            placeholder="密码"
-            focusBorderColor="inherit"
-            border="none"
-          />
-        </InputGroup>
-      </Box>
-      <HStack w="100%" justifyContent="space-between" my="15px">
-        <Checkbox size="sm" defaultChecked fontSize="13px">
-          记住我
-        </Checkbox>
-        <Text fontSize="13px">登陆遇到问题？</Text>
-      </HStack>
-      <Button
-        w="100%"
-        borderRadius="30px"
-        bg="#3194d0"
-        color="#fff"
-        _hover={{ bg: '#187cb7' }}
-      >
-        登录
-      </Button>
-      <HStack spacing="50px" mt="50px" mb="10px">
-        <Text fontSize="12px">社交账号登录</Text>
-      </HStack>
-      <HStack spacing="30px" mt="20px">
-        <FaWeibo color="#e95244" fontSize="25px" cursor="pointer" />
-        <FaWeixin color="#00bb29" fontSize="28px" cursor="pointer" />
-        <FaQq color="#498ad5" fontSize="24px" cursor="pointer" />
-      </HStack>
-    </>
-  );
-}
-
-function Register() {
-  return (
-    <>
-      <Box
-        border="1px solid #c8c8c8"
-        borderRadius="4px"
-        bg="rgba(181,181,181,0.1)"
-        w="100%"
-      >
-        <InputGroup>
-          <InputLeftElement h="50px" children={<HiUser />} />
-          <Input
-            type="text"
-            h="50px"
-            fontSize="13px"
-            placeholder="你的昵称"
-            focusBorderColor="inherit"
-            border="none"
-            borderBottom="1px solid #c8c8c8"
-            borderRadius="0px"
-          />
-        </InputGroup>
-        <InputGroup>
-          <InputLeftElement h="50px" children={<HiPhone />} />
-          <Input
-            type="number"
-            maxLength="11"
-            h="50px"
-            fontSize="13px"
-            placeholder="手机号"
-            focusBorderColor="inherit"
-            borderBottom="1px solid #c8c8c8"
-            border="none"
-            borderRadius="0px"
-          />
-        </InputGroup>
-        <InputGroup>
-          <InputLeftElement h="50px" children={<HiLockClosed />} />
-          <Input
-            type="password"
-            h="50px"
-            fontSize="13px"
-            placeholder="密码"
-            focusBorderColor="inherit"
-            border="none"
-          />
-        </InputGroup>
-      </Box>
-      <Button
-        w="100%"
-        borderRadius="30px"
-        bg="#42c02e"
-        color="#fff"
-        _hover={{ bg: '#3db922' }}
-        mt="20px"
-        mb="10px"
-      >
-        注册
-      </Button>
-      <Text fontSize="12px" lineHeight="20px">
-        点击 “注册” 即表示您同意并愿意遵守简书
-      </Text>
-      <Text fontSize="12px" lineHeight="20px">
-        <Link color="#3194d0">用户协议</Link>和
-        <Link color="#3194d0">隐私政策</Link>
-      </Text>
-      <HStack spacing="50px" mt="50px" mb="10px">
-        <Text fontSize="12px">社交账号直接注册</Text>
-      </HStack>
-      <HStack spacing="30px" mt="20px">
-        <FaWeixin color="#00bb29" fontSize="28px" cursor="pointer" />
-        <FaQq color="#498ad5" fontSize="24px" cursor="pointer" />
-      </HStack>
-    </>
-  );
-}
-
 function App() {
   const [currentTab, setCurrentTab] = useState('login');
   const handleChangeTab = useCallback(flag => {
     setCurrentTab(flag);
-  });
+  }, []);
   var activeStyle = {
     color: '#ea6f5a',
     fontWeight: 700,
